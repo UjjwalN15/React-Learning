909ed93a61c8897bb38786848c048ab10cd2f445
Ujjwal Neupane <itsmeujjwal725@gmail.com>
2025-01-22 18:15:42 +0545

    Inline and Conditional CSS learned

--- CssInReact/src/components/Series.jsx
+++ CssInReact/src/components/Series.jsx
@@ -1,14 +1,17 @@
 export const SeriesCard = (props)=>{
+    const button_style = {
+        backgroundColor: `${props.series.rating >= 8.0 ? "green" : "red"}`,
+        color:"white",
+    }
     return (
-        <li key={props} className="card">
+        <li key={props} className="card" style={{listStyleType:"none"}}>
             <img src={props.series.image_url} alt={props.series.name} />
             <h1>{props.series.name}</h1>
-            <p>{props.series.description}</p>
-            <p>{props.series.rating}</p>
+            <p> <span className={`rating ${props.series.rating >=8.0 ? "super_hit" : "average"}`}>{props.series.rating}</span></p>
             <p>{props.series.genre}</p>
             <p>[{props.series.cast}]</p>
             <a href={props.series.watch_url}>
-                <button>Watch Now</button>
+                <button style={button_style}>Watch Now</button>
             </a>
         </li>
     );
